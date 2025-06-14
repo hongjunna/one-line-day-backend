b0a88160644545f50ee795eb77a2b20ff2d024e8
hongjunna <jun@junlab.xyz>
2025-06-14 20:53:37 +0900

    feat:Account part 기본세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import AccountController from './account/account.controller';
+import AccountService from './account/account.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, AccountController],
+  providers: [AppService, AccountService],
 })
 export class AppModule {}
